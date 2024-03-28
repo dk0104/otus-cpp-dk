f6a30812a44d4ebd23edbacfe465b01a934707dc
Denis Keksel <Denis Keksel>
2024-03-28 19:28:45 +0400

    first custom allocator atemt

--- src/lib/hpp/customAllocator.h
+++ src/lib/hpp/customAllocator.h
@@ -1,41 +1,94 @@
 #pragma once
 #include <cstddef>
-#include <limits>
 #include <memory>
+#include <stdexcept>
+#include <vector>
 
-// teplate<typename T> class CustomAllocator {
-// public:
-//   using value_type = T;
-//   CustomAllocator() = default;
+template <typename T> class CustomAllocator {
+public:
+  using value_type = T;
+  using pointer = T *;
+  using const_pointer = const T *;
+  using reference = T &;
+  using const_reference = const T &;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
 
-//   template <typename U> CustomAllocator(const CustomAllocator<U> &) noexcept
-//   {}
+  CustomAllocator(std::size_t initialPoolSize = 100)
+      : poolSize(initialPoolSize), allocated(0) {
+    pool.reserve(initialPoolSize);
+    for (size_type i = 0; i < initialPoolSize; ++i) {
+      pool.push_back(new char[sizeof(T)]);
+    }
+  }
 
-//   T *allocate(std::size_t n) {
-//     if (n > max_sizes())
-//       throw std::bad_alloc();
-//     return static_cast<T *>(::operator new(n * sizeof(T)));
-//   }
-//   void deallocate(T *p, std::size_t) noexcept { ::operator delete(p); }
+  template <typename U>
+  CustomAllocator(const CustomAllocator<U> &other)
+      : poolSize(other.poolSize), allocated(other.allocated) {
+    pool.reserve(poolSize);
+    for (size_type i = 0; i < allocated; ++i) {
+      pool.push_back(new char[sizeof(T)]);
+    }
+  }
 
-//   teplate<typename... args> void construct(T *p, args &&...args) {
-//     new (p) T(std::forward<args>(args)...);
-//   }
-//   void destroy(T *p) { p->~T(); }
+  ~CustomAllocator() {
+    for (auto ptr : pool) {
+      delete[] ptr;
+    }
+  }
 
-//   std::size_t max_size() const noexcept {
-//     return std::numeric_limits<std::size_t>::max() / sizeof(T);
-//   }
-// };
+  T *allocate(std::size_t n) {
+    if (n > poolSize - allocated) {
+      size_type newPoolSize = poolSize * 2; // Double the pool size
+      while (n > newPoolSize - allocated) {
+        newPoolSize *= 2; // Keep doubling until enough space is available
+      }
+      for (size_type i = poolSize; i < newPoolSize; ++i) {
+        pool.push_back(new char[sizeof(T)]);
+      }
+      poolSize = newPoolSize;
+    }
+    T *ptr = reinterpret_cast<T *>(pool[allocated]);
+    allocated += n;
+    return ptr;
+  }
 
-// template <typename T, typename U>
-// bool operator==(const CustomAllocator<T> &,
-//                 const CustomAllocator<U> &) noexcept {
-//   return true;
-// }
+  void deallocate(T *ptr, std::size_t n) noexcept {
+    // Deallocation is a no-op in this allocator
+  }
 
-// template <typename T, typename U>
-// bool operator!=(const CustomAllocator<T> &,
-//                 const CustomAllocator<U> &) noexcept {
-//   return false;
-// }
+  template <typename U, typename... Args>
+  void construct(U *ptr, Args &&...args) {
+    new (ptr) U(std::forward<Args>(args)...);
+  }
+
+  template <typename U> void destroy(U *ptr) noexcept { ptr->~U(); }
+
+  size_type max_size() const noexcept { return size_type(-1) / sizeof(T); }
+
+  pointer address(reference x) const noexcept { return std::addressof(x); }
+
+  const_pointer address(const_reference x) const noexcept {
+    return std::addressof(x);
+  }
+
+  template <class U> struct rebind {
+    typedef CustomAllocator<U> other;
+  };
+
+  size_type poolSize;
+  std::vector<void *> pool;
+  std::size_t allocated;
+};
+
+template <typename T, typename U>
+bool operator==(const CustomAllocator<T> &p1,
+                const CustomAllocator<U> &p2) noexcept {
+  return p1.pool == p2.pool;
+}
+
+template <typename T, typename U>
+bool operator!=(const CustomAllocator<T> &p1,
+                const CustomAllocator<U> &p2) noexcept {
+  return p1.pool == p2.pool; // Always return false since it's stateless
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
+#include "lib/hpp/customAllocator.h"
 #include "lib/hpp/ipfilter.h"
 #include "lib/hpp/lib.h"
-
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <memory>
 #include <sstream>
+
+template <typename T> constexpr T factorial(T n) {
+  return (n == 0) ? 1 : n * factorial(n - 1);
+}
 
 int main(int, char **) {
   std::cout << "Version: " << version() << std::endl;
   std::ifstream file("ip_filter.tsv");
 
+  /// Ip Filter
   IpFilter ipf(file);
   std::cout << ipf;
+
+  /// Custom Allocator
+  CustomAllocator<int> ca(10);
+  std::map<int, int, std::less<int>, CustomAllocator<std::pair<const int, int>>>
+      map;
+  for (int i = 0; i < 10; i++) {
+    map[i] = factorial(i);
+  }
+
+  for (auto [key, value] : map) {
+    std::cout << key << " " << value << std::endl;
+  }
 
   return version();
 }
